028e5305242180c053827230d0e5011f8c7daa6d
nautimon <nautisignup@gmail.com>
2026-05-12 05:38:46 +0000

    Separate lines 2 ways

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log(/* Your code here */);
+console.log("Hey Jude, don't let me down\nYou have found her, now go and get her\nRemember to let her into your heart\nThen you can start to make it better\n");
+console.log(`Hey Jude, don't let me down
+You have found her, now go and get her
+Remember to let her into your heart
+Then you can start to make it better`);
